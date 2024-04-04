44b798b9a6265025e2bde7f57b92e8c3b6e0a325
Kauê Evangelista da Silva Aguiar <kauesilva38@hotmail.com>
2024-04-04 11:34:23 -0300

    comit

--- src/components/SafeContainer.js
+++ src/components/SafeContainer.js
@@ -1,8 +1,33 @@
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
+
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function SafeContainer({ children }) {
-  return <SafeAreaView style={styles.container}>{children}</SafeAreaView>;
+  const [fontsLoaded, fontError] = useFonts({
+    Barlow: require("../../assets/fonts/Barlow-Regular.ttf"),
+    NotoSans: require("../../assets/fonts/Comfortaa-Regular.ttf"),
+  });
+
+  const aoAtualizarLayout = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <SafeAreaView style={styles.container} onLayout={aoAtualizarLayout}>
+      {children}
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
--- src/screens/Inicial.js
+++ src/screens/Inicial.js
@@ -1,49 +1,51 @@
 import { TouchableOpacity, View, StyleSheet, Text } from "react-native";
 import SafeContainer from "../components/SafeContainer";
 
-
-export default function Inicial({navigation}){
-    return(
-        <SafeContainer>
-            <View style={styles.container}>
-                <TouchableOpacity style={styles.botaoCadastrar} onPress={()=>{
-                    navigation.navigate("Cadastro")
-                }}>
-                    <Text style={styles.textoBotao}>cadastrar</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.botaoLogin}>
-                    <Text style={styles.textoBotao}>login</Text>
-                </TouchableOpacity>
-            </View>
-
-        </SafeContainer>
-    )
+export default function Inicial({ navigation }) {
+  return (
+    <SafeContainer>
+      <View style={styles.container}>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={styles.botaoCadastrar}
+          onPress={() => {
+            navigation.navigate("Cadastro");
+          }}
+        >
+          <Text style={styles.textoBotao}>cadastrar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.botaoLogin} activeOpacity={0.8}>
+          <Text style={styles.textoBotao}>login</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeContainer>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        gap: 15,
-    },
+  container: {
+    gap: 15,
+  },
 
-    botaoCadastrar: {
-        height: 50,
-        borderRadius: 10,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "green",
-    },
+  botaoCadastrar: {
+    height: 50,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "green",
+  },
 
-    botaoLogin: {
-        height: 50,
-        borderRadius: 10,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "blue",
-    },
+  botaoLogin: {
+    height: 50,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "blue",
+  },
 
-    textoBotao: {
-        fontSize: 16,
-        textTransform: "uppercase",
-        fontWeight: "bold",
-    }
-})
+  textoBotao: {
+    fontSize: 16,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+  },
+});
