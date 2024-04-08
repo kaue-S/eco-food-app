03ee75f6b257544067b078cdf008039e5642b564
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-04-08 11:25:59 -0300

    feat: Alterações dos cards dos produtos

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,6 +5,7 @@ import {
   StyleSheet,
   TouchableOpacity,
   Pressable,
+  ScrollView,
 } from "react-native";
 import arrayProdutos from "../api/arrayDeProdutos";
 import Produto from "../components/Produto";
@@ -12,25 +13,33 @@ import Produto from "../components/Produto";
 export default function Home({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Bem-vindo à página Home!</Text>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.goBack()}
-      >
-        <Text style={styles.buttonText}>Sair</Text>
-      </TouchableOpacity>
-      <Pressable
-        style={styles.button}
-        onPress={() => navigation.navigate("Carrinho")}
-      >
-        <Text style={styles.buttonText}>Carrinho</Text>
-      </Pressable>
-
-      <View style={styles.viewProdutos}>
-        {arrayProdutos.map((itemProduto) => {
-          return <Produto produto={itemProduto} />;
-        })}
-      </View>
+      <ScrollView>
+        <Text style={styles.text}>Bem-vindo à página Home!</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.goBack()}
+        >
+          <Text style={styles.buttonText}>Sair</Text>
+        </TouchableOpacity>
+        <View>
+          <Text style={styles.text}>Destaques</Text>
+          <ScrollView
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={styles.viewProdutos}
+          >
+            {arrayProdutos.map((itemProduto) => {
+              return <Produto key={itemProduto.id} produto={itemProduto} />;
+            })}
+          </ScrollView>
+        </View>
+        <Pressable
+          style={styles.button}
+          onPress={() => navigation.navigate("Carrinho")}
+        >
+          <Text style={styles.buttonText}>Carrinho</Text>
+        </Pressable>
+      </ScrollView>
     </View>
   );
 }
@@ -38,9 +47,10 @@ export default function Home({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#f0f0f0",
+    marginTop: 10,
+    padding: 12,
   },
   text: {
     fontSize: 20,
@@ -54,5 +64,11 @@ const styles = StyleSheet.create({
   buttonText: {
     color: "white",
     fontSize: 16,
+  },
+  viewProdutos: {
+    justifyContent: "space-between",
+    gap: 20,
+    marginBottom: 18,
+    padding: 30,
   },
 });
