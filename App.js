af6d67cef53ebf21ecd7dd1d1c56895a6694faec
Phelipe <phelipesenac@gmail.com>
2024-04-08 11:17:12 -0300

    .
    .

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
+import { StyleSheet, SafeAreaView, StatusBar } from "react-native";
 import CadastroUsuario from "./src/screens/CadastroUsuario";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Inicial from "./src/screens/Inicial";
 import LoginUsuario from "./src/screens/LoginUsuario";
 import Home from "./src/screens/Home";
-import { ScreenStackHeaderLeftView } from "react-native-screens";
 import Carrinho from "./src/screens/Carrinho";
 import AddCarrinho from "./src/screens/AddCarrinho";
-// import {  } from "react-native-safe-area-context";
 
 const Stack = createNativeStackNavigator();
 
