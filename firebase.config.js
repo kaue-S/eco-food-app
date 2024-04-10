2cfb266fcfe196ea5c712bef3a55a9580f50db4c
kaue-S <kauesilva38@hotmail.com>
2024-04-10 19:56:05 -0300

    comit

--- firebase.config.js
+++ firebase.config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
 // Importando recursos da biblioteca de Autenticação
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
