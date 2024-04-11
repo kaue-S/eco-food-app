18dc795ea2c97a14e469d7b236d05297dcef071b
kaue-S <kauesilva38@hotmail.com>
2024-04-10 23:57:43 -0300

    Update CadastroUsuario.js

--- src/screens/CadastroUsuario.js
+++ src/screens/CadastroUsuario.js
@@ -9,7 +9,7 @@ import {
 } from "react-native";
 import SafeContainer from "../components/SafeContainer";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { ref, set } from "firebase/database";
+import {push, ref, set } from "firebase/database";
 import { auth, db } from "../../firebase.config";
 
 export default function CadastroUsuario({ navigation }) {
@@ -17,86 +17,50 @@ export default function CadastroUsuario({ navigation }) {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
 
-  const cadastrar = async () => {
-    if (!email || !senha || !nome) {  
-      Alert.alert("Atenção!", "Preencha nome, e-mail e senha!");
-      return;
-    }
 
-    try {
-      const contaUsuario = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        senha
-      );
+    function cadastrar() {
+      const usuarioRef = push(ref(db, 'usuarios')); // Gera um novo ID único
+      const usuarioKey = usuarioRef.key; // Obtém o ID único gerado
 
-      if (contaUsuario.user) {
-        await updateProfile(auth.currentUser, { displayName: nome });
-        console.log(contaUsuario.user.displayName);
-      }
+      set(ref(db, 'usuarios/' + usuarioKey), {
+        nome: nome,
+        email: email,
+        senha: senha,
+      }); 
 
-      Alert.alert("Cadastro", "Seu cadastro foi concluído com sucesso!", [
-        {
-          style: "cancel",
-          text: "Ficar aqui mesmo",
-          onPress: () => {
-            return;
-          },
-        },
-        {
-          style: "default",
-          text: "Ir para a área logada",
-          onPress: () => navigation.replace("Home"),
-        },
-      ]);
-    } catch (error) {
-      // console.error(error.code);
-      let mensagem;
-      switch (error.code) {
-        case "auth/email-already-in-use":
-          mensagem = "E-mail já cadastrado!";
-          break;
-        case "auth/weak-password":
-          mensagem = "Senha fraca (mínimo de 6 caracteres)";
-          break;
-        case "auth/invalid-email":
-          mensagem = "Endereço de e-mail inválido!";
-          break;
-        default:
-          mensagem = "Houve um erro, tente mais tarde!";
-          break;
-      }
-      Alert.alert("Ops!", mensagem);
-    }
-  };
+  } 
+      
 
   return (
     <SafeContainer>
       <View style={styles.formulario}>
         <View style={styles.campoCadastro}>
           <TextInput
+          value={nome}
             placeholder="Nome Completo"
             style={styles.input}
             keyboardType="default"
-            onChangeText={(valor) => setNome(valor)}
+            onChangeText={(nome) => {setNome(nome)}}
           />
         </View>
         <View style={styles.campoCadastro}>
           <TextInput
+          value={email}
             placeholder="E-mail"
             style={styles.input}
             keyboardType="email-address"
-            onChangeText={(valor) => setEmail(valor)}
+            onChangeText={(email) => {setEmail(email)}}
           />
         </View>
 
         <View style={styles.campoCadastro}>
           <TextInput
+          value={senha}
             placeholder="Senha"
             style={styles.input}
             keyboardType="default"
             secureTextEntry
-            onChangeText={(valor) => setSenha(valor)}
+            onChangeText={(senha) => {setSenha(senha)}}
           />
         </View>
 
