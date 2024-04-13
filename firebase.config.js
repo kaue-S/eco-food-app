ca76b329b3c8041be25e54d277d09b4737b73540
kaue-S <kauesilva38@hotmail.com>
2024-04-12 23:07:14 -0300

    Update firebase.config.js

--- firebase.config.js
+++ firebase.config.js
@@ -1,22 +1,28 @@
 import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
 // Importando recursos da biblioteca de Autenticação
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAhIXdDG9fcfFN3oDCXcLQe9E0SQLataT4",
-  authDomain: "eco-food-db.firebaseapp.com",
-  projectId: "eco-food-db",
-  storageBucket: "eco-food-db.appspot.com",
-  messagingSenderId: "905163517071",
-  appId: "1:905163517071:web:7d78cd7a2deef0e93e54fa",
+  apiKey: "AIzaSyCOkjn8NPtreTkAsfTLcdcEKTZaEDlihC0",
+  authDomain: "eco-food-app.firebaseapp.com",
+  projectId: "eco-food-app",
+  storageBucket: "eco-food-app.appspot.com",
+  messagingSenderId: "93024734440",
+  appId: "1:93024734440:web:4a7ca7de38e3f739404d5d",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // Configurando o recurso de autenticação para uso em outras partes
-export const auth = initializeAuth(app, {
+const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
+
+//Iniciação banco de dados
+const db = getDatabase(app);
+
+export { auth, db };
