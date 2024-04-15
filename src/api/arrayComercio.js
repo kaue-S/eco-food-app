5b731c7947c6ed9bf71d34eb2b57509c44de77fd
Kauê Evangelista da Silva Aguiar <kauesilva38@hotmail.com>
2024-04-15 09:23:44 -0300

    Create arrayComercio.js

---
+++ src/api/arrayComercio.js
@@ -0,0 +1,101 @@
+const comercio = [
+  {
+    nome: "Mourão",
+    endereco: {
+      rua: "Rua das Lamentações",
+      numero: "11",
+    },
+    tipoComercio: "Hortifruti",
+    filtro: "Hortifruti",
+  },
+  {
+    nome: "Pets da aline",
+    endereco: {
+      rua: "Rua da timidez",
+      numero: "12",
+    },
+    tipoComercio: "petshop",
+    filtro: "petshop",
+  },
+  {
+    nome: "TanakaMotos",
+    endereco: {
+      rua: "Rua da motoca falante",
+      numero: "13",
+    },
+    tipoComercio: "concessionária",
+    filtro: "concessionária",
+  },
+  {
+    nome: "Buenos Codesign",
+    endereco: {
+      rua: "Rua de Quem não dorme",
+      numero: "14",
+    },
+    tipoComercio: "informatica",
+    filtro: "informatica",
+  },
+  {
+    nome: "Vitão mercadão",
+    endereco: {
+      rua: "Rua da feira",
+      numero: "15",
+    },
+    tipoComercio: "Mercado",
+    filtro: "Mercado",
+  },
+  {
+    nome: "Lanchelielson",
+    endereco: {
+      rua: "Rua do oriente",
+      numero: "16",
+    },
+    tipoComercio: "Lanches",
+    filtro: "Lanches",
+  },
+  {
+    nome: "Tanakara Sushi",
+    endereco: {
+      rua: "Rua dos doidos",
+      numero: "17",
+    },
+    tipoComercio: "Restaurante",
+    filtro: "Restaurante",
+  },
+  {
+    nome: "Zimbo Star",
+    endereco: {
+      rua: "Rua do sebastião",
+      numero: "18",
+    },
+    tipoComercio: "Hortifruti",
+    filtro: "Hortifruti",
+  },
+  {
+    nome: "Padaria Doo Doo",
+    endereco: {
+      rua: "Rua melani",
+      numero: "19",
+    },
+    tipoComercio: "Padaria",
+    filtro: "Padaria",
+  },
+  {
+    nome: "Emporio mocoto",
+    endereco: {
+      rua: "Rua dos comerciantes",
+      numero: "20",
+    },
+    tipoComercio: "Açougue",
+    filtro: "Açougue",
+  },
+  {
+    nome: "Padaria do Leleco",
+    endereco: {
+      rua: "Rua dos Pães",
+      numero: "21",
+    },
+    tipoComercio: "Padaria",
+    filtro: "Padaria",
+  },
+];
