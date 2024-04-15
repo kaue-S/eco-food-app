a78e4229198f7a8d43412b9f02d2dea7fcccead0
Kauê Evangelista da Silva Aguiar <kauesilva38@hotmail.com>
2024-04-15 11:30:24 -0300

    Update arrayComercio.js

--- src/api/arrayComercio.js
+++ src/api/arrayComercio.js
@@ -1,100 +1,61 @@
-const comercio = [
+[
   {
     nome: "Mourão",
-    endereco: {
-      rua: "Rua das Lamentações",
-      numero: "11",
-    },
+    endereco: "Rua das Lamentações, 11",
     tipoComercio: "Hortifruti",
     filtro: "Hortifruti",
   },
   {
     nome: "Pets da aline",
-    endereco: {
-      rua: "Rua da timidez",
-      numero: "12",
-    },
+    endereco: "Rua da timidez, 12",
     tipoComercio: "petshop",
     filtro: "petshop",
   },
   {
-    nome: "TanakaMotos",
-    endereco: {
-      rua: "Rua da motoca falante",
-      numero: "13",
-    },
-    tipoComercio: "concessionária",
-    filtro: "concessionária",
-  },
-  {
-    nome: "Buenos Codesign",
-    endereco: {
-      rua: "Rua de Quem não dorme",
-      numero: "14",
-    },
+    nome: "Buenos",
+    endereco: "Rua de Quem não dorme, 14",
     tipoComercio: "informatica",
-    filtro: "informatica",
+    filtro: "restaurante",
   },
   {
     nome: "Vitão mercadão",
-    endereco: {
-      rua: "Rua da feira",
-      numero: "15",
-    },
+    endereco: "Rua da feira, 15",
     tipoComercio: "Mercado",
     filtro: "Mercado",
   },
   {
     nome: "Lanchelielson",
-    endereco: {
-      rua: "Rua do oriente",
-      numero: "16",
-    },
+    endereco: "Rua do oriente, 16",
     tipoComercio: "Lanches",
     filtro: "Lanches",
   },
   {
     nome: "Tanakara Sushi",
-    endereco: {
-      rua: "Rua dos doidos",
-      numero: "17",
-    },
+    endereco: "Rua dos doidos, 17",
     tipoComercio: "Restaurante",
     filtro: "Restaurante",
   },
   {
     nome: "Zimbo Star",
-    endereco: {
-      rua: "Rua do sebastião",
-      numero: "18",
-    },
+    endereco: "Rua do sebastião, 18",
     tipoComercio: "Hortifruti",
     filtro: "Hortifruti",
   },
   {
     nome: "Padaria Doo Doo",
-    endereco: {
-      rua: "Rua melani",
-      numero: "19",
-    },
+    endereco: "Rua melani, 19",
     tipoComercio: "Padaria",
     filtro: "Padaria",
   },
   {
     nome: "Emporio mocoto",
-    endereco: {
-      rua: "Rua dos comerciantes",
-      numero: "20",
-    },
+    endereco: "Rua dos comerciantes, 20",
     tipoComercio: "Açougue",
     filtro: "Açougue",
   },
   {
     nome: "Padaria do Leleco",
-    endereco: {
-      rua: "Rua dos Pães",
-      numero: "21",
-    },
+    endereco: "Rua dos Pães, 21",
     tipoComercio: "Padaria",
     filtro: "Padaria",
   },
