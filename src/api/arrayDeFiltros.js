d70ab0f530de59fd3dbaa56fd9084124fb383207
Kauê Evangelista da Silva Aguiar <kauesilva38@hotmail.com>
2024-04-16 09:19:48 -0300

    Create arrayDeFiltros.js

---
+++ src/api/arrayDeFiltros.js
@@ -0,0 +1,125 @@
+import bovino from "../../assets/images/fotoCategorias/filtroBovinos.png";
+import leite from "../../assets/images/fotoCategorias/filtroLeites.png";
+import pao from "../../assets/images/fotoCategorias/filtroPao.png";
+import sobremesas from "../../assets/images/fotoCategorias/filtroSorvete.png";
+import doces from "../../assets/images/fotoCategorias/filtroDoces.png";
+import aves from "../../assets/images/fotoCategorias/filtroAves.png";
+import legumes from "../../assets/images/fotoCategorias/filtroLegumes.png";
+import cafe from "../../assets/images/fotoCategorias/filtroCafes.png";
+import frutas from "../../assets/images/fotoCategorias/filtroFrutas.png";
+import queijos from "../../assets/images/fotoCategorias/filtroQueijos.png";
+import cereais from "../../assets/images/fotoCategorias/filtroCereais.png";
+import peixes from "../../assets/images/fotoCategorias/filtroPeixes.png";
+import massas from "../../assets/images/fotoCategorias/filtroPizzas.png";
+import manteigas from "../../assets/images/fotoCategorias/filtroManteigas.png";
+import lanches from "../../assets/images/fotoCategorias/filtroLanches.png";
+import iogurtes from "../../assets/images/fotoCategorias/filtroIogurtes.png";
+import bebidas from "../../assets/images/fotoCategorias/filtroBebidas.png";
+import chas from "../../assets/images/fotoCategorias/filtroChas.png";
+import salada from "../../assets/images/fotoCategorias/filtroSalada.png";
+import semGlutem from "../../assets/images/fotoCategorias/filtroSemGlutem.png";
+
+const arrayFiltros = [
+  {
+    id: 1,
+    nome: "Bovinos",
+    foto: bovino,
+  },
+  {
+    id: 2,
+    nome: "Leites",
+    foto: leite,
+  },
+  {
+    id: 3,
+    nome: "Pão",
+    foto: pao,
+  },
+  {
+    id: 4,
+    nome: "Sobremesas",
+    foto: sobremesas,
+  },
+  {
+    id: 5,
+    nome: "Doces",
+    foto: doces,
+  },
+  {
+    id: 6,
+    nome: "Aves",
+    foto: aves,
+  },
+  {
+    id: 7,
+    nome: "Legumes",
+    foto: legumes,
+  },
+  {
+    id: 8,
+    nome: "Cafés",
+    foto: cafe,
+  },
+  {
+    id: 9,
+    nome: "Frutas",
+    foto: frutas,
+  },
+  {
+    id: 10,
+    nome: "Queijos",
+    foto: queijos,
+  },
+  {
+    id: 11,
+    nome: "Cereais",
+    foto: cereais,
+  },
+  {
+    id: 12,
+    nome: "Peixes",
+    foto: peixes,
+  },
+  {
+    id: 13,
+    nome: "Massas",
+    foto: massas,
+  },
+  {
+    id: 14,
+    nome: "Manteigas",
+    foto: manteigas,
+  },
+  {
+    id: 15,
+    nome: "Lanches",
+    foto: lanches,
+  },
+  {
+    id: 16,
+    nome: "Iorgurtes",
+    foto: iogurtes,
+  },
+  {
+    id: 17,
+    nome: "Bebidas",
+    foto: bebidas,
+  },
+  {
+    id: 18,
+    nome: "Chás",
+    foto: chas,
+  },
+  {
+    id: 19,
+    nome: "Saladas",
+    foto: salada,
+  },
+  {
+    id: 20,
+    nome: "Sem Glutem",
+    foto: semGlutem,
+  },
+];
+
+export default arrayFiltros;
