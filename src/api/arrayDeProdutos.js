37668c83ba2c8071ef6410bdcf637f18cf5fce88
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-04-16 09:20:38 -0300

    feat:Removendo a tabela nutricional

--- src/api/arrayDeProdutos.js
+++ src/api/arrayDeProdutos.js
@@ -8,48 +8,7 @@ const arrayProdutos = [
     foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmbQ-Wow9xHOqs0lTuSQ7KfOxkd386tIFZuadiQ4Bvag&s",
     categoria: "hortifruti",
     mercado_id: 1,
-    tabelaNutricional: {
-      calorias: {
-        nome: " Calorias (valor energético)",
-        quantidade: "46.95 kcal",
-        vd: "2.35%",
-      },
-      carboidratos_liquidos: {
-        nome: "Carboidratos líquidos",
-        quantidade: "8.55 g",
-        vd: "-",
-      },
-      carboidratos: {
-        nome: "Carboidratos",
-        quantidade: "8.55 g",
-        vd: "2.85%",
-      },
-      proteinas: {
-        nome: "Proteínas",
-        quantidade: "1.16 g",
-        vd: "0.39%",
-      },
-      gorduras_totais: {
-        nome: "Gorduras totais",
-        quantidade: "1.01 g",
-        vd: "1.83%",
-      },
-      gorduras_saturadas: {
-        nome: "Gorduras saturadas",
-        quantidade: "0.63 g",
-        vd: "2.86%",
-      },
-      fibra_alimentar: {
-        nome: "Fibra alimentar",
-        quantidade: "0.00 g",
-        vd: "0.00%",
-      },
-      sodio: {
-        nome: "Sódio",
-        quantidade: "14.10 mg",
-        vd: "0.59%",
-      },
-    },
+
     descricao: "Glutém, aveia, trigo, cevada,protéina do leite",
     destaque: "não",
   },
@@ -61,48 +20,7 @@ const arrayProdutos = [
     foto: "https://upload.wikimedia.org/wikipedia/commons/3/31/Banana_Icon.png",
     categoria: "hortifruti",
     mercado_id: 2,
-    tabelaNutricional: {
-      calorias: {
-        nome: " Calorias (valor energético)",
-        quantidade: "46.95 kcal",
-        vd: "2.35%",
-      },
-      carboidratos_liquidos: {
-        nome: "Carboidratos líquidos",
-        quantidade: "8.55 g",
-        vd: "-",
-      },
-      carboidratos: {
-        nome: "Carboidratos",
-        quantidade: "8.55 g",
-        vd: "2.85%",
-      },
-      proteinas: {
-        nome: "Proteínas",
-        quantidade: "1.16 g",
-        vd: "0.39%",
-      },
-      gorduras_totais: {
-        nome: "Gorduras totais",
-        quantidade: "1.01 g",
-        vd: "1.83%",
-      },
-      gorduras_saturadas: {
-        nome: "Gorduras saturadas",
-        quantidade: "0.63 g",
-        vd: "2.86%",
-      },
-      fibra_alimentar: {
-        nome: "Fibra alimentar",
-        quantidade: "0.00 g",
-        vd: "0.00%",
-      },
-      sodio: {
-        nome: "Sódio",
-        quantidade: "14.10 mg",
-        vd: "0.59%",
-      },
-    },
+
     descricao: "Glutém, aveia, trigo, cevada,protéina do leite",
     destaque: "não",
   },
@@ -114,48 +32,7 @@ const arrayProdutos = [
     foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGd6tezGGe0bq4ViF3E0Z2yNJsvWpk3hRQz-fZvIpkvw&s",
     categoria: "Leites",
     mercado_id: 3,
-    tabelaNutricional: {
-      calorias: {
-        nome: " Calorias (valor energético)",
-        quantidade: "46.95 kcal",
-        vd: "2.35%",
-      },
-      carboidratos_liquidos: {
-        nome: "Carboidratos líquidos",
-        quantidade: "8.55 g",
-        vd: "-",
-      },
-      carboidratos: {
-        nome: "Carboidratos",
-        quantidade: "8.55 g",
-        vd: "2.85%",
-      },
-      proteinas: {
-        nome: "Proteínas",
-        quantidade: "1.16 g",
-        vd: "0.39%",
-      },
-      gorduras_totais: {
-        nome: "Gorduras totais",
-        quantidade: "1.01 g",
-        vd: "1.83%",
-      },
-      gorduras_saturadas: {
-        nome: "Gorduras saturadas",
-        quantidade: "0.63 g",
-        vd: "2.86%",
-      },
-      fibra_alimentar: {
-        nome: "Fibra alimentar",
-        quantidade: "0.00 g",
-        vd: "0.00%",
-      },
-      sodio: {
-        nome: "Sódio",
-        quantidade: "14.10 mg",
-        vd: "0.59%",
-      },
-    },
+
     descricao: "Glutém, aveia, trigo, cevada,protéina do leite",
     destaque: "sim",
   },
@@ -167,48 +44,7 @@ const arrayProdutos = [
     foto: "https://upload.wikimedia.org/wikipedia/commons/8/81/Goiaba-fruta.jpg",
     categoria: "hortifruti",
     mercado_id: 4,
-    tabelaNutricional: {
-      calorias: {
-        nome: " Calorias (valor energético)",
-        quantidade: "46.95 kcal",
-        vd: "2.35%",
-      },
-      carboidratos_liquidos: {
-        nome: "Carboidratos líquidos",
-        quantidade: "8.55 g",
-        vd: "-",
-      },
-      carboidratos: {
-        nome: "Carboidratos",
-        quantidade: "8.55 g",
-        vd: "2.85%",
-      },
-      proteinas: {
-        nome: "Proteínas",
-        quantidade: "1.16 g",
-        vd: "0.39%",
-      },
-      gorduras_totais: {
-        nome: "Gorduras totais",
-        quantidade: "1.01 g",
-        vd: "1.83%",
-      },
-      gorduras_saturadas: {
-        nome: "Gorduras saturadas",
-        quantidade: "0.63 g",
-        vd: "2.86%",
-      },
-      fibra_alimentar: {
-        nome: "Fibra alimentar",
-        quantidade: "0.00 g",
-        vd: "0.00%",
-      },
-      sodio: {
-        nome: "Sódio",
-        quantidade: "14.10 mg",
-        vd: "0.59%",
-      },
-    },
+
     descricao: "agrotóxicos",
     destaque: "sim",
   },
